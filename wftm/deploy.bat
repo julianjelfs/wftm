ad814c3f56854f4e9acce2e733174617be0f7d4c
Julian Jelfs <julian.jelfs@gmail.com>
2013-04-02 08:08:14 +0100

    renamed possessions to equipment

---
+++ wftm/deploy.bat
@@ -0,0 +1,3 @@
+c:\windows\system32\xcopy.exe "..\index.html" @C:\Users\julianj\Dropbox\Public\wftm\" /y
+c:\windows\system32\xcopy.exe "..\Scripts\app.js" "C:\Users\julianj\Dropbox\Public\wftm\Scripts" /y
+c:\windows\system32\xcopy.exe "..\Content\app.css" "C:\Users\julianj\Dropbox\Public\wftm\Content" /y
